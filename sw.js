9f91c9377a0acafa659b1c755d6e2ae125ae2577
Rrodher <cruzricardorh@hotmail.com>
2025-09-16 16:11:15 -0700

    sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'mandadito-v7';
+const urlsToCache = [
+  'mandadito_v9.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
